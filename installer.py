507c6a36a7539fe5e88ca5b17dc0cc497de7dde9
radlih <156937535+radlih@users.noreply.github.com>
2024-02-08 06:10:27 +0800

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,54 @@
+import os
+print ("""
+                                             
+                             \033[95m⣿⣿⣿⠿⠿⣿⣿⡿⢋⣶⣶⣬⣙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                             \033[95⣿⡿⢡⣿⣷⣶⣦⣥⣿⣿⣿⣿⣿⣷⣮⡛⢿⣿⣿⣿⣿⣿⣿⣿
+                             \033[95⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢮⡙⣿⣿⣯⢐⡎⣿
+                             \033[95⣿⢹⣿⣿⣿⣿⣿⣿⡿⣡⡬⢿⣿⣿⣿⣶⣶⣼⣦⠥⣖⣩⣾⣿
+                             \033[95⣿⢸⣿⣿⣿⡿⣿⣿⣿⣿⠇⣌⢛⣻⣿⣿⣟⣛⣿⣧⠹⣿⣿⣿
+                             \033[95⠏⣼⣿⣿⢏⣾⣿⣟⣩⣶⣶⣿⣿⣿⣿⣿⡟⡿⢸⡿⣡⣿⣿⣿
+                             \033[95⣼⣿⣿⠇⣼⣿⣿⢸⠋⠁⠉⢽⣿⣿⣿⣟⣠⣤⣆⢃⢻⣿⣿⣿
+                             \033[95⣿⣿⣿⣼⣿⣿⣿⡞⣿⣿⣷⣾⣿⣿⣿⣿⡿⠟⠛⠸⢦⣙⡋⣿
+                             \033[95⣿⣿⣿⠹⣿⣿⡿⠗⣈⣭⣭⣭⣉⠻⡟⣩⣶⣾⣿⣿⣶⡙⣱⣿
+                             \033[95⣿⣿⣿⣷⣌⡛⠠⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⢸⣿
+                             \033[95⣿⣿⣿⣿⢏⣴⣧⣴⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣱⣶⣴⡜⢸⣿
+                             \033[95⣿⣿⣿⢃⣾⣿⣿⣿⡷⠉⢿⣿⣿⣿⣿⣿⣿⢰⣾⣿⣿⣧⢸⣿
+                          
+""")
+print("""\33[0;32m[1] RUN\n[2] CANCEL\nCancel or Run?""")
+c = input(">>>: ")
+if c == "1":
+    os.system("npm install gradient-string")
+    os.system("npm install path")
+    os.system("npm install cheerio")
+    os.system("npm install axios")
+    os.system("npm install header-generator")
+    os.system("npm install request")
+    os.system("npm install net")
+    os.system("npm install cloudscraper")
+    os.system("npm install cluster")
+    os.system("npm install https")
+    os.system("npm install http2")
+    os.system("npm install user-agents")
+    os.system("npm install crypto")
+    os.system("npm install tls")
+    os.system("npm install fake-useragent")
+    os.system("npm install fs")
+    os.system("npm install colors")
+    os.system("npm install set-cookie-parser")
+    os.system("npm install url")
+    os.system("npm install randomstring")
+    os.system("npm install random-ua")
+    os.system("npm install sync-request")
+    os.system("npm i child_process")
+    os.system("npm i request")
+    os.system("npm i cloudscraper")
+    os.system("npm i events")
+    os.system("npm install events")
+    os.system("npm install colors")
+    os.system("npm install random-useragent")
+    os.system("npm install http")
+
+elif c == "2":
+    os.system("clear")
+print ("DONE!")
